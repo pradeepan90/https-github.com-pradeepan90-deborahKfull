0ca9cab11829a203b5ae03e3848c92efe0a7b4fd
pradeepan90 <pradeepan90@gmail.com>
2020-04-15 10:44:33 +0530

    1 file modified, 1 added, 1 deleted

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -9,57 +9,13 @@ import { IProduct } from './product';
   providedIn: 'root'
 })
 export class ProductService {
-
-  // getProducts(): IProduct[] {
-  //   return [
-  // {
-  //   "productId": 1,
-  //   "productName": "Leaf Rake",
-  //   "productCode": "GDN-0011",
-  //   "releaseDate": "March 19, 2019",
-  //   "description": "Leaf rake with 48-inch wooden handle.",
-  //   "price": 19.95,
-  //   "starRating": 3.2,
-  //   "imageUrl": "assets/images/leaf_rake.png"
-  // },
-  // {
-  //   "productId": 2,
-  //   "productName": "Garden Cart",
-  //   "productCode": "GDN-0023",
-  //   "releaseDate": "March 18, 2019",
-  //   "description": "15 gallon capacity rolling garden cart",
-  //   "price": 32.99,
-  //   "starRating": 4.2,
-  //   "imageUrl": "assets/images/garden_cart.png"
-  // },
-  // {
-  //   "productId": 5,
-  //   "productName": "Hammer",
-  //   "productCode": "TBX-0048",
-  //   "releaseDate": "May 21, 2019",
-  //   "description": "Curved claw steel hammer",
-  //   "price": 8.9,
-  //   "starRating": 4.8,
-  //   "imageUrl": "assets/images/hammer.png"
-  // },
-  // {
-  //   "productId": 8,
-  //   "productName": "Saw",
-  //   "productCode": "TBX-0022",
-  //   "releaseDate": "May 15, 2019",
-  //   "description": "15-inch steel blade hand saw",
-  //   "price": 11.55,
-  //   "starRating": 3.7,
-  //   "imageUrl": "assets/images/saw.png"
-  // }
-  // ];
-  // }
-  // If using Stackblitz, replace the url with this line
+ // If using Stackblitz, replace the url with this line
   // because Stackblitz can't find the api folder.
-  // private productUrl = 'assets/products/products.json';
+  private productUrl = 'https://my-json-server.typicode.com/pradeepan90/demo/posts'
+  //'assets/products/products.json';
 
 
-  private productUrl = 'api/products/products.json';
+  // private productUrl = 'api/products/products.json';
 
   constructor(private http: HttpClient) { }
 
